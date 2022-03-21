d6df7b682841272d593a55cb3eacb600a84a0c20
correiarangel <correiarangel@bol.com.br>
2022-03-21 14:36:42 -0300

    Fix:limpando codigo

--- lib/app/modules/intoxianime/components/listview_intoxianime.dart
+++ lib/app/modules/intoxianime/components/listview_intoxianime.dart
@@ -27,7 +27,7 @@ class ListViewIntoxianime extends StatefulWidget {
 class _ListViewIntoxianimeState extends State<ListViewIntoxianime> {
   final ScrollController _scrollController = ScrollController();
   // ignore: prefer_final_fields
-  late List<IntoxianimeModel> _infinityAnimeModels = [];
+  List<IntoxianimeModel> _infinityAnimeModels = [];
   addList(List<IntoxianimeModel> animeModels) {}
   bool isAddInList = false;
   @override
@@ -96,28 +96,6 @@ class _ListViewIntoxianimeState extends State<ListViewIntoxianime> {
       );
     }
 
-/*     if (state is IntoxianimeStateSuccess) {
-      listReturn = Padding(
-        padding: const EdgeInsets.only(top: 38.0),
-        child: ListView.builder(
-          itemCount: state.animeModels.length,
-          itemBuilder: (_, index) {
-            final anime = state.animeModels[index];
-            return CardAnimeCustom(
-              themaController: themeController,
-              imgHeaderPath: 'images/glasses.png',
-              textHeader: 'Anime',
-              textButton: 'Ver Mais',
-              textLink: 'Acessar',
-              description: anime.title,
-              urlLink: anime.link,
-              guidUrl: anime.guidUrl,
-            );
-          },
-        ),
-      );
-    }
- */
     if (state is IntoxianimeStateSuccess) {
       for (var element in state.animeModels) {
         _infinityAnimeModels.add(element);
@@ -158,98 +136,3 @@ class _ListViewIntoxianimeState extends State<ListViewIntoxianime> {
   }
 }
 
-/*
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../shared/controllers/theme_controller.dart';
-import '../blocs/intoxianime_bloc.dart';
-
-import '../states/intoxianime_state.dart';
-import 'card_anime.dart';
-
-class ListViewIntoxianime extends StatelessWidget {
-  final ThemeController themeController;
-  final Function eventFetch;
-  const ListViewIntoxianime({
-    required this.themeController,
-    required this.eventFetch,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final top = MediaQuery.of(context).size.height;
-    Widget listReturn = Container();
-
-    return BlocBuilder<IntoxianimeBloc, IntoxianimeState>(
-      builder: (context, state) {
-        if (state is IntoxianimeStateInitial) {
-          listReturn = Padding(
-            padding: EdgeInsets.only(top: top / 3),
-            child: const Center(
-              child: Text(
-                'Nenhum artigo encontrado',
-                style: TextStyle(fontSize: 18.0),
-              ),
-            ),
-          );
-        }
-        if (state is IntoxianimeStateLoading) {
-          listReturn = Padding(
-            padding: EdgeInsets.only(top: top / 3),
-            child: const Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-
-        if (state is IntoxianimeStateException) {
-          listReturn = Padding(
-            padding: EdgeInsets.only(top: top / 3),
-            child: Center(
-              child: TextButton.icon(
-                onPressed: () {
-                  //bloc.add();
-                  eventFetch();
-                },
-                icon: const Icon(
-                  Icons.refresh_outlined,
-                  size: 28.0,
-                ),
-                label: const Text(
-                  'Erro ao buscar Anime',
-                  style: TextStyle(fontSize: 22.0),
-                ),
-              ),
-            ),
-          );
-        }
-
-        if (state is IntoxianimeStateSuccess) {
-          listReturn = Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 38.0),
-              child: ListView.builder(
-                itemCount: state.animeModels.length,
-                itemBuilder: (_, index) {
-                  final anime = state.animeModels[index];
-                  return CardAnime(
-                    number: '$index',
-                    title: anime.title,
-                    themeController: themeController,
-                    guidUrl: anime.guidUrl,
-                    link: anime.link,
-                  );
-                },
-              ),
-            ),
-          );
-        }
-        return listReturn;
-      },
-    );
-  }
-}
-
- */
